0e6731bc91b33ae6568e761bd19f8506ca25b99d
Elizabeth A. Fujita <syp009@c28-0113-34.ad.mtu.edu>
2019-07-12 13:26:22 -0400

    Bank App Codes

---
+++ app.js
@@ -0,0 +1,12 @@
+function card1(){
+    document.getElementById("card").src="https://ck-content.imgix.net/res/content/creditcards/ccfifththirdbank/ccfifththird02_big.png?auto=compress%2Cformat"
+}
+function card2(){
+    document.getElementById("card").src="https://ck-content.imgix.net/res/content/creditcards/merrickban/ccmerrickbank1301_big.png?auto=compress%2Cformat"
+}
+function card3(){
+    document.getElementById("card").src="https://www.us.hsbc.com/content/dam/hsbc/us/images/credit-card-comparison/cc_prem_card_img_542x347.png"
+}
+function card4(){
+    document.getElementById("card").src="https://content.usaa.com/mcontent/static_assets/Media/bank-featurette_PreferredCash_Front_Emb_VisaSig_iso_promo.png?cacheid=300947267_p"
+}
